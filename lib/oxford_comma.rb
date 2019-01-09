e607a75b479d88c6ba93ab61f6e332d2c54fb6bb
Yong Nam Kang <j.yongnamkang@gmail.com>
2019-01-09 23:54:45 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,20 @@
 def oxford_comma(array)
-
+  if array.count == 1
+    return array[0]
+  elsif array.count == 2
+    return array.join(" and ")
+  else
+    string = array.first
+    # correction, 1st element already concatenated in line above.
+    array.shift
+    
+    array.each do |element|
+      if element == array.last
+        string << ", and #{element}"
+      else
+        string << ", #{element}"
+      end
+    end
+  end
+    string
 end
